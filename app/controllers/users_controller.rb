0769e28c1388ab39eaf61faf6fdf427187de1cb5
alexandr <89227049120@mail.ru>
2021-11-17 16:54:32 +0500

    add helper amount questions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
-    # Создаём массив из двух болванок пользователей. Вызываем метод # User.new, который создает модель, не записывая её в базу.
+    # Создаём массив из двух болванок пользователей. Вызываем метод # User.new, который создает модель, 
+    # не записывая её в базу.
     # У каждого юзера мы прописали id, чтобы сымитировать реальную
     # ситуацию – иначе не будет работать хелпер путей
     @users = [
@@ -8,13 +9,11 @@ class UsersController < ApplicationController
         id: 1,
         name: 'Vadim',
         username: 'installero',
-        avatar_url: 'https://secure.gravatar.com/avatar/' \
-          '71269686e0f757ddb4f73614f43ae445?s=100'
+        avatar_url: 'https://secure.gravatar.com/avatar/71269686e0f757ddb4f73614f43ae445?s=100'
       ),
-      User.new(id: 2,
-      name: 'Misha',
-      username: 'aristofun'
-      )
+      User.new(id: 2, name: 'Misha', username: 'aristofun'),
+      User.new(id: 3, name: 'Sergey', username: 'Serg'),
+      User.new(id: 4, name: 'Artem', username: 'Tema', avatar_url: 'avatar2.jpg')
     ]
   end
 
@@ -27,10 +26,16 @@ class UsersController < ApplicationController
   def show
     @user = User.new(
       name: 'Vadim',
-      username: 'installero'
+      username: 'installero',
+      avatar_url: 'https://secure.gravatar.com/avatar/71269686e0f757ddb4f73614f43ae445?s=100'
     )
     @questions = [
-      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016'))
+      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'It is number 2', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'It is number 3', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'It is number 4', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'It is number 5', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'It is number 6', created_at: Date.parse('27.03.2016'))
     ]
     @new_question = Question.new
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,16 @@ module ApplicationHelper
       asset_path 'avatar.jpg'
     end
   end
+
+  def amount_questions(amount)
+    result = amount % 10 unless (11..19).include?(amount % 100)
+
+    def_form_word = 
+      case result
+      when 1 then 'вопрос'
+      when 2..4 then 'вопроса'
+      else 'вопросов'
+      end
+    "#{amount} #{def_form_word}"
+  end 
 end
