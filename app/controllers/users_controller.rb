91d3ad988732b7eb169986989c745ce3bdd94383
alexandr <89227049120@mail.ru>
2021-11-18 11:38:09 +0500

    add count amount answers/questions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,11 +30,11 @@ class UsersController < ApplicationController
       avatar_url: 'https://secure.gravatar.com/avatar/71269686e0f757ddb4f73614f43ae445?s=100'
     )
     @questions = [
-      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
-      Question.new(text: 'It is number 2', created_at: Date.parse('27.03.2016')),
-      Question.new(text: 'It is number 3', created_at: Date.parse('27.03.2016')),
-      Question.new(text: 'It is number 4', created_at: Date.parse('27.03.2016')),
-      Question.new(text: 'It is number 5', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016'), answer: 'I am fine!'),
+      Question.new(text: 'It is number 2', created_at: Date.parse('27.03.2016'), answer: 'Answer 2'),
+      Question.new(text: 'It is number 3', created_at: Date.parse('27.03.2016'), answer: 'Answer 3'),
+      Question.new(text: 'It is number 4', created_at: Date.parse('27.03.2016'), answer: 'Answer 4'),
+      Question.new(text: 'It is number 5', created_at: Date.parse('27.03.2016'), answer: 'Answer 5'),
       Question.new(text: 'It is number 6', created_at: Date.parse('27.03.2016'))
     ]
     @new_question = Question.new
