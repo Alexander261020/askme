ed8574f6fe9398fa3390e7443aa808bb62b3f40a
alexandr <89227049120@mail.ru>
2021-11-18 17:52:34 +0500

    add unigue field username and email

--- db/migrate/20211112093727_add_email_password_to_user.rb
+++ db/migrate/20211112093727_add_email_password_to_user.rb
@@ -1,6 +1,8 @@
 class AddEmailPasswordToUser < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :email, :string
+    add_index :users, :email, unique: true
+    add_index :users, :username, unique: true
     add_column :users, :password_hash, :string
     add_column :users, :password_salt, :string
 
