e164e51e756c3e546879998cf92d1edebf7634c7
alexandr <89227049120@mail.ru>
2021-11-20 14:42:56 +0500

    Revert "transfer code from view in controller"
    This reverts commit 84e5855a7f27e447cf8efbe807e0dc2a3d75019b.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   end
 
   def show
+<<<<<<< HEAD
   end
 
   private
@@ -26,5 +27,21 @@ class UsersController < ApplicationController
     # набор инпутов. Ничего лишнего, кроме них, в пользователя не попадёт
     params.require(:user).permit(:email, :password, :password_confirmation,
                                  :name, :username, :avatar_url)
+=======
+    @user = User.new(
+      name: 'Vadim',
+      username: 'installero',
+      avatar_url: 'https://secure.gravatar.com/avatar/71269686e0f757ddb4f73614f43ae445?s=100'
+    )
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016'), answer: 'I am fine!'),
+      Question.new(text: 'It is number 2', created_at: Date.parse('27.03.2016'), answer: 'Answer 2'),
+      Question.new(text: 'It is number 3', created_at: Date.parse('27.03.2016'), answer: 'Answer 3'),
+      Question.new(text: 'It is number 4', created_at: Date.parse('27.03.2016'), answer: 'Answer 4'),
+      Question.new(text: 'It is number 5', created_at: Date.parse('27.03.2016'), answer: 'Answer 5'),
+      Question.new(text: 'It is number 6', created_at: Date.parse('27.03.2016'))
+    ]
+    @new_question = Question.new
+>>>>>>> parent of 84e5855... transfer code from view in controller
   end
 end
