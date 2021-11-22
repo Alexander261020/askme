99f381f604deedf3e108fa49e48f128bddd37e6f
alexandr <89227049120@mail.ru>
2021-11-22 15:00:37 +0500

    user + sessions controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,39 @@
 class UsersController < ApplicationController
+
+  before_action :load_user, except: [:index, :new, :create]
+
+  before_action :authorize_user, except: [:index, :new, :create, :show]
+
   def index
     @users = User.all
   end
 
   def new
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
     @user = User.new
   end
 
   def create
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
     @user = User.new(user_params)
     
     if @user.save
       # Если удачно, отправляем пользователя на главную с помощью метода redirect_to
       # с сообщением
       redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+    else
+      render 'new'
+    end
+  end
+
+  def update
+
+    if @user.update(user_params)
+      # Если удачно, отправляем пользователя на главную с помощью метода redirect_to
+      # с сообщением
+      redirect_to user_path(@user), notice: 'Данные обновлены'
+    else
+      render 'edit'
     end
   end
 
@@ -21,21 +41,25 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.new(
-      name: 'Vadim',
-      username: 'installero',
-      avatar_url: 'https://secure.gravatar.com/avatar/71269686e0f757ddb4f73614f43ae445?s=100'
-    )
+    # берём вопросы у найденного юзера
+    @questions = @user.questions.order(created_at: :desc)
+    # Для формы нового вопроса создаём заготовку, вызывая build у результата вызова метода @user.questions.
+    @new_question = @user.questions.build
 
-    @new_question = Question.new
-
-    @questions = Question.all
-    @amount_questions = Question.all.count
-    @amount_answers = Question.where.not(:answer => nil).count
-    @questions_without_answer = Question.where(:answer => nil).count
+    @amount_questions = @user.questions.all.count
+    @amount_answers = @user.questions.where.not(:answer => nil).count
+    @questions_without_answer = @user.questions.where(:answer => nil).count
   end
 
   private
+
+  def authorize_user
+    reject_user unless @user == current_user
+  end
+
+  def load_user
+    @user ||= User.find params[:id]
+  end
 
   def user_params
     # берём объект params, потребуем у него иметь ключ
