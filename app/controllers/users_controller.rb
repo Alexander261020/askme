e30cc0e5b843fc8921586f53ba8b1e641f52647d
alexandr <89227049120@mail.ru>
2021-11-22 10:49:26 +0500

    dates from BD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,21 +22,13 @@ class UsersController < ApplicationController
       username: 'installero',
       avatar_url: 'https://secure.gravatar.com/avatar/71269686e0f757ddb4f73614f43ae445?s=100'
     )
-    @questions = [
-      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016'), answer: 'I am fine!'),
-      Question.new(text: 'It is number 2', created_at: Date.parse('27.03.2016'), answer: 'Answer 2'),
-      Question.new(text: 'It is number 3', created_at: Date.parse('27.03.2016'), answer: 'Answer 3'),
-      Question.new(text: 'It is number 4', created_at: Date.parse('27.03.2016'), answer: 'Answer 4'),
-      Question.new(text: 'It is number 5', created_at: Date.parse('27.03.2016'), answer: 'Answer 5'),
-      Question.new(text: 'It is number 6', created_at: Date.parse('27.03.2016'))
-    ]
+
     @new_question = Question.new
 
-    @up_block = "У #{@user.name} а есть #{helpers.get_declination(@questions.count, 'вопрос', 'вопроса', 'вопросов')},"
-=begin     <%= get_declination(@questions.map(&:answer).compact.count, 'ответ', 'ответа', 'ответов') %>,
-    и <%= get_declination(@questions.map(&:answer).count(nil), 'вопрос', 'вопроса', 'вопросов') %> без ответа." 
-=end
-
+    @questions = Question.all
+    @amount_questions = Question.all.count
+    @amount_answers = Question.where.not(:answer => nil).count
+    @questions_without_answer = Question.where(:answer => nil).count
   end
 
   private
