6c090aa166027533e728ab33a89195b5706c2b27
alexandr <89227049120@mail.ru>
2021-11-28 14:08:40 +0500

    error migration

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,10 +6,12 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    if current_user == nil
-      redirect_to new_user_path, notice: 'Для того что бы задать вопрос нужно
-        зарегистрироваться или залогиниться!' and return
+=begin
+    unless current_user
+      redirect_to new_user_path, notice: 'Для того что бы задать вопрос нужно зарегистрироваться или залогиниться!'
+      return
     end
+=end
 
     @question = Question.new(question_params)
     @question.author = current_user
