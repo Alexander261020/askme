063399ba85716f47def5e1fa64560b99de79fd48
alexandr <89227049120@mail.ru>
2021-12-14 20:49:59 +0500

    before reCaptcha

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
 
   resources :users
   resources :tags, only: [:show]
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
   resources :questions, except: [:show, :new, :index]
 
-  get 'sign_up' => 'users#new'
+=begin   get 'sign_up' => 'users#new'
   get 'log_out' => 'sessions#destroy'
-  get 'log_in' => 'sessions#new'
+  get 'log_in' => 'sessions#new' 
+=end
 end
