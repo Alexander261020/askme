230be17c43a289063401f32270bf3a5c5f545092
alexandr <89227049120@mail.ru>
2022-01-19 15:29:14 +0500

    raturn

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,10 +1,6 @@
 class TagsController < ApplicationController
   def show
-    if Tag.exists?(text: params[:id])
-      @tag = Tag.where(text: params[:id]).first
-      @questions = @tag.questions
-    else
-      redirect_to root_path, notice: "Хештег #{params[:id]} не обнаружен!"
-    end
+    @tag = Tag.where(text: params[:id]).first
+    @questions = @tag.questions
   end
 end
